685937f560e59b11b818e7249bc68a254b54fae0
Sinclair Fuh <sfuh1@umbc.edu>
2024-03-01 19:39:14 -0500

    fixed formatting errors in previous commit

--- src/app/Components/Intro_Text/intro.tsx
+++ src/app/Components/Intro_Text/intro.tsx
@@ -16,8 +16,8 @@ export default function Intro(data){
               <div id="texttitle" >About Me</div>
               <div id="textcontent">
                 I&apos;m <span style={{color: 'blue'}}>Sinclair</span>, a graduate student whose studies intersect Artificial Intelligence with Software Enginnering.
-                 <span style={{color: 'blue'}}> Brazenkind</span> is the alias I use on github. I just finished my Masters' in Computer Science, alongside a one year fellowship
-                 at Devcom ARL doing web development. I'm currently looking for software engineering/artificial intelligence roles, but also have an interest in virtual reality.
+                 <span style={{color: 'blue'}}> Brazenkind</span> is the alias I use on github. I just finished my Masters&apos; in Computer Science, alongside a one year fellowship
+                 at Devcom ARL doing web development. I&apos;m currently looking for software engineering/artificial intelligence roles, but also have an interest in virtual reality.
               </div>
             </div>
         </div>

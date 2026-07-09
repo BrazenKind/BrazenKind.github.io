3cbee05737c573cfe6e809d59230b7f004254751
Sinclair Fuh <sfuh1@umbc.edu>
2026-07-08 22:26:11 -0400

    saving this version as legacy branch

--- src/app/Components/Intro_Text/intro.tsx
+++ src/app/Components/Intro_Text/intro.tsx
@@ -1,5 +1,6 @@
 import '../css/intro.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Intro(data){
 
@@ -15,9 +16,12 @@ export default function Intro(data){
             <div id="textbox">
               <div id="texttitle" >About Me</div>
               <div id="textcontent">
-                I&apos;m <span style={{color: 'var(--rgb-start)'}}>Sinclair</span>, a graduate student whose studies intersect Artificial Intelligence with Software Enginnering.
-                 <span style={{color: 'var(--rgb-start)'}}> Brazenkind</span> is the alias I use on github. I just finished my Masters&apos; in Computer Science, alongside a one year fellowship
-                 at Devcom ARL doing web development. I&apos;m currently looking for software engineering/artificial intelligence roles, but also have an interest in virtual reality.
+                I&apos;m <span style={{color: 'var(--rgb-start)'}}>Sinclair</span>, a software developer whose applied python full stack development to a myriad of government solutions, from <span style={{color: 'var(--rgb-end)'}}> developing AI interfaces for the Devcom Army Reseach Laboratory</span> 
+                 to <span style={{color: 'var(--rgb-end)'}}>developing massively scalable data platforms at Lyteforge.</span>
+                 <span style={{color: 'var(--rgb-start)'}}> Brazenkind </span> is the alias I use on github. 
+                 <br></br>
+                  <br></br>I'm actively seeking full stack opportunities that align with my previous experience. If you're a recruiter who'd like to know more about my work experience or simply someone who 
+                  would like to reach out, <Link href="#contact_me">click here to be directed to my contact form.</Link>
               </div>
             </div>
         </div>
